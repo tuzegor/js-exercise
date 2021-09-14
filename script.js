e529d9d2fb12b342e6c630f911b18a0a32eca420
Yehor Tuz <tuzegor@gmail.com>
2021-09-15 00:07:31 +0300

    task 5

---
+++ script.js
@@ -0,0 +1,15 @@
+const userText = document.querySelector('.input-task-5');
+const answer = document.querySelector('.answer-task-5');
+answer.textContent = 'Введите число';
+userText.addEventListener('input', check);
+
+function check(evt) {
+  let numb = evt.target.value;
+  console.log(numb);
+  if (numb >= 15 && numb <= 90) {
+    userText.classList.add('change-color');
+    return (answer.textContent = `${numb} -- Входит`);
+  }
+  userText.classList.remove('change-color');
+  answer.textContent = `${numb} -- Попробуй другое`;
+}
