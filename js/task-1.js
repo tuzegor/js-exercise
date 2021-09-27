cb4ae389b2a0b765618bdab1b584b09becf16790
Yehor Tuz <tuzegor@gmail.com>
2021-09-27 09:57:39 +0300

    task-1 update

--- js/task-1.js
+++ js/task-1.js
@@ -3,14 +3,27 @@ const resultButton = document.querySelector('.result-button-task-1');
 const result = document.querySelector('.result-task-1');
 
 resultButton.addEventListener('click', checkDate);
-
+date.addEventListener('keydown', checkDateOnEnter);
 function checkDate() {
   console.log(date.value);
-  if (date.value % 4 === 0) {
-    result.classList.add('success');
-    result.textContent = `Тебе повезло. ${date.value} - Это высокосный год`;
+  console.log(isNaN(date.value));
+  console.log(date.value % 4 === 0);
+  if (!isNaN(date.value)) {
+    if (date.value % 4 === 0) {
+      result.classList.add('success');
+      result.textContent = `Тебе повезло. ${date.value} - Это высокосный год`;
+    } else {
+      result.classList.remove('success');
+      result.textContent = `${date.value} - Это не высокосный год`;
+    }
   } else {
     result.classList.remove('success');
-    result.textContent = `${date.value} - Это не высокосный год`;
+    result.textContent = `${date.value} - Это не дата рождения`;
+  }
+}
+
+function checkDateOnEnter(event) {
+  if (event.code === 'Enter') {
+    checkDate();
   }
 }
