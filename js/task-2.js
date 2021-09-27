89c18f1863a889aab59f73ef02dbbaf6ed8abfa0
Yehor Tuz <tuzegor@gmail.com>
2021-09-27 23:58:42 +0300

    task-2 done

---
+++ js/task-2.js
@@ -0,0 +1,26 @@
+const number = document.querySelector('.input-task-2');
+const resultButton = document.querySelector('.result-button-task-2');
+const result = document.querySelector('.result-task-2');
+
+resultButton.addEventListener('click', checkNumber);
+number.addEventListener('keydown', checkNumberOnEnter);
+
+function checkNumber() {
+  const random = Math.floor(Math.random() * 10 + 1);
+
+  if (number.value > 0 && number.value <= 10) {
+    if (Number(number.value) === random) {
+      result.classList.add('success');
+      result.textContent = `Поздравляю ты угадал, число компьютера ${random}`;
+    } else {
+      result.classList.remove('success');
+      result.textContent = `Ты проиграл, число компьютера ${random}`;
+    }
+  }
+}
+
+function checkNumberOnEnter(e) {
+  if (e.code === 'Enter') {
+    checkNumber();
+  }
+}
