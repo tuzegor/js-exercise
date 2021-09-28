8eec7e2f6c5f33211ee8f581a40872935c361b36
Yehor Tuz <tuzegor@gmail.com>
2021-09-29 00:12:13 +0300

    task-9 логика

---
+++ js/task-9.js
@@ -0,0 +1,21 @@
+const choice = document.getElementById('choice-input-task-9');
+const resultBtn = document.querySelector('.result-button-task-9');
+const result = document.querySelector('.result-task-9');
+resultBtn.addEventListener('click', startGame);
+
+function startGame() {
+  result.style.backgroundColor = 'white';
+
+  console.dir(result);
+  const random = Math.random();
+  if (random < 0.33) {
+    console.log('< 0,33');
+    result.textContent = 'Камінь';
+  } else if (random >= 0.33 && random < 0.66) {
+    console.log('0,33 - 0,66');
+    result.textContent = 'Ножиці';
+  } else if (random >= 0.66) {
+    console.log('> 0,66');
+    result.textContent = 'Папір';
+  }
+}
