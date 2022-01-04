10a82d838e5b8d1c52bdb68513614a4b3b652c4b
Yehor Tuz <tuzegor@gmail.com>
2022-01-04 18:29:53 +0200

    Update task-9.js

--- js/task-9.js
+++ js/task-9.js
@@ -1,21 +1,21 @@
-// const choice = document.getElementById('choice-input-task-9');
-// const resultBtn = document.querySelector('.result-button-task-9');
-// const result = document.querySelector('.result-task-9');
-// resultBtn.addEventListener('click', startGame);
+const choice = document.getElementById('choice-input-task-9');
+const resultBtn = document.querySelector('.result-button-task-9');
+const result = document.querySelector('.result-task-9');
+resultBtn.addEventListener('click', startGame);
 
-// function startGame() {
-//   result.classList.add('answer-task-9');
+function startGame() {
+  result.classList.add('answer-task-9');
 
-//   console.dir(result);
-//   const random = Math.random();
-//   if (random < 0.33) {
-//     console.log('< 0,33');
-//     result.textContent = 'Камінь';
-//   } else if (random >= 0.33 && random < 0.66) {
-//     console.log('0,33 - 0,66');
-//     result.textContent = 'Ножиці';
-//   } else if (random >= 0.66) {
-//     console.log('> 0,66');
-//     result.textContent = 'Папір';
-//   }
-// }
+  console.dir(result);
+  const random = Math.random();
+  if (random < 0.33) {
+    console.log('< 0,33');
+    result.textContent = 'Камінь';
+  } else if (random >= 0.33 && random < 0.66) {
+    console.log('0,33 - 0,66');
+    result.textContent = 'Ножиці';
+  } else if (random >= 0.66) {
+    console.log('> 0,66');
+    result.textContent = 'Папір';
+  }
+}
